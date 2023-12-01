85d9440f068478ea2d581e4972c5faaeb8117ded
leesuyeon116 <suyeon873088@naver.com>
2023-12-01 13:15:34 +0900

    스캐너 추가

--- src/ch03_scanner/Ex01_Scanner.java
+++ src/ch03_scanner/Ex01_Scanner.java
@@ -11,6 +11,17 @@ public class Ex01_Scanner {
 		System.out.print("숫자를 입력하세요: ");
 		int num1 = scanner.nextInt();		
 		System.out.println(num1);
-	}
+		
+		System.out.print("이름을 입력하세요: ");
+		String name = scanner.next();
+		System.out.println(name);
+		
+		scanner.nextLine();
+		
+		System.out.println("주소를 입력하세요: ");
+		String address = scanner.nextLine();
+		System.out.println(address);
+		
+		}
 
 }
