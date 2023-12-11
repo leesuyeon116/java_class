6dfcb40cbdb668bb0c242e9590b9d28edc1d99fc
leesuyeon116 <suyeon873088@naver.com>
2023-12-11 21:41:01 +0900

    주말숙제 풀이

--- src/ch09_array/Ex09_ArrayEx.java
+++ src/ch09_array/Ex09_ArrayEx.java
@@ -4,47 +4,15 @@ import java.util.Scanner;
 
 public class Ex09_ArrayEx {
     public static void main(String[] args) {
-        /**
-         * ------------------------------------------------------
-         * 1.학생수입력 | 2.점수입력 | 3.점수목록 | 4.분석 | 5.종료
-         * ------------------------------------------------------
-         * 선택> 1
-         * 학생수> 3
-         * ------------------------------------------------------
-         * 1.학생수입력 | 2.점수입력 | 3.점수목록 | 4.분석 | 5.종료
-         * ------------------------------------------------------
-         * 선택> 2
-         * 1번 학생 점수: 90
-         * 2번 학생 점수: 80
-         * 3번 학생 점수: 77
-         * ------------------------------------------------------
-         * 1.학생수입력 | 2.점수입력 | 3.점수목록 | 4.분석 | 5.종료
-         * ------------------------------------------------------
-         * 선택> 3
-         * 1번 학생 점수: 90
-         * 2번 학생 점수: 80
-         * 3번 학생 점수: 77
-         * ------------------------------------------------------
-         * 1.학생수입력 | 2.점수입력 | 3.점수목록 | 4.분석 | 5.종료
-         * ------------------------------------------------------
-         * 선택> 4
-         * 최고점수: 90
-         * 평균점수: 82.33333333333333
-         * 1등: 90
-         * 2등: 80
-         * 3등: 77
-         * ------------------------------------------------------
-         * 1.학생수입력 | 2.점수입력 | 3.점수목록 | 4.분석 | 5.종료
-         * ------------------------------------------------------
-         * 선택>
-         * ------------------------------------------------------
-         */
+        // 스캐너 객체를 선언한다.
         Scanner scan = new Scanner(System.in);
+        // boolean 타입의 run 변수를 선언해서 true를 대입한다.
         boolean run = true;
+        // int 타입의 studentNum 변수를 선언해서 0을 대입한다.
         int studentNum = 0;
         int selectNo = 0;
 
-        int[] scores = null;
+        int[] scores = null; // scores라는 배열 이름만 선언
 
         while (run) {
             System.out.println("--------------------------------------------------------------");
@@ -52,29 +20,29 @@ public class Ex09_ArrayEx {
             System.out.println("--------------------------------------------------------------");
             System.out.print("선택> ");
             selectNo = scan.nextInt();
-
+            // 전역변수이긴 하지만 while문이 실행될 때마다 다시 선언되어 배열로 활용 불가
 //            int[] scores = null;
             if (selectNo == 1) {
-
+                // 학생수 입력 받고 해당 수 크기를 갖는 배열로 선언
                 System.out.print("학생수>");
                 studentNum = scan.nextInt();
-
+                // 입력받은 학생수만큼의 크기를 갖는 배열 객체로 선언(new)
                 scores = new int[studentNum];
-
+                // 지역변수로 선언하면 else if 에서는 사용 불가
 //                int[] scores = new int[studentNum];
             } else if (selectNo == 2) {
-
+                // 배열에 점수 데이터 저장
                 for (int i = 0; i < scores.length; i++) {
                     System.out.print((i + 1) + "번 학생 점수: ");
                     scores[i] = scan.nextInt();
                 }
             } else if (selectNo == 3) {
-
+                // 저장된 데이터 출력
                 for (int i = 0; i < scores.length; i++) {
                     System.out.println((i + 1) + "번 학생 점수: " + scores[i]);
                 }
             } else if (selectNo == 4) {
-
+                // 최댓값, 평균값 계산 및 출력
                 int sum = 0, max = 0;
                 double avg = 0.0;
                 for (int i = 0; i < scores.length; i++) {
@@ -88,7 +56,7 @@ public class Ex09_ArrayEx {
                 System.out.println("최고점수: " + max);
                 System.out.println("평균점수: " + avg);
 
-
+                // 내림차순 정렬 및 출력
                 int temp = 0;
                 for (int i = 0; i < scores.length; i++) {
                     for (int j = i + 1; j < scores.length; j++) {
@@ -103,7 +71,7 @@ public class Ex09_ArrayEx {
                     System.out.println((i + 1) + "등: " + scores[i]);
                 }
             } else if (selectNo == 5) {
-
+                // 종료
                 run = false;
             }
         }
