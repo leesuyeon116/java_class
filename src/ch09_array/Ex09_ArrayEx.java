9259638dadd4ab59bee33aef17465ec9422d6ced
leesuyeon116 <suyeon873088@naver.com>
2023-12-11 21:36:59 +0900

    주말숙제 풀이

--- src/ch09_array/Ex09_ArrayEx.java
+++ src/ch09_array/Ex09_ArrayEx.java
@@ -1,5 +1,7 @@
 package ch09_array;
 
+import java.util.Scanner;
+
 public class Ex09_ArrayEx {
     public static void main(String[] args) {
         /**
@@ -37,5 +39,73 @@ public class Ex09_ArrayEx {
          * 선택>
          * ------------------------------------------------------
          */
+        Scanner scan = new Scanner(System.in);
+        boolean run = true;
+        int studentNum = 0;
+        int selectNo = 0;
+
+        int[] scores = null;
+
+        while (run) {
+            System.out.println("--------------------------------------------------------------");
+            System.out.println("1.학생수입력 | 2.점수입력 | 3.점수목록 | 4.분석 | 5.종료");
+            System.out.println("--------------------------------------------------------------");
+            System.out.print("선택> ");
+            selectNo = scan.nextInt();
+
+//            int[] scores = null;
+            if (selectNo == 1) {
+
+                System.out.print("학생수>");
+                studentNum = scan.nextInt();
+
+                scores = new int[studentNum];
+
+//                int[] scores = new int[studentNum];
+            } else if (selectNo == 2) {
+
+                for (int i = 0; i < scores.length; i++) {
+                    System.out.print((i + 1) + "번 학생 점수: ");
+                    scores[i] = scan.nextInt();
+                }
+            } else if (selectNo == 3) {
+
+                for (int i = 0; i < scores.length; i++) {
+                    System.out.println((i + 1) + "번 학생 점수: " + scores[i]);
+                }
+            } else if (selectNo == 4) {
+
+                int sum = 0, max = 0;
+                double avg = 0.0;
+                for (int i = 0; i < scores.length; i++) {
+                    if (max < scores[i]) {
+                        max = scores[i];
+                    }
+                    sum += scores[i];
+                }
+                avg = sum / scores.length;
+                avg = (double) sum / scores.length;
+                System.out.println("최고점수: " + max);
+                System.out.println("평균점수: " + avg);
+
+
+                int temp = 0;
+                for (int i = 0; i < scores.length; i++) {
+                    for (int j = i + 1; j < scores.length; j++) {
+                        if (scores[i] < scores[j]) {
+                            temp = scores[i];
+                            scores[i] = scores[j];
+                            scores[j] = temp;
+                        }
+                    }
+                }
+                for (int i = 0; i < scores.length; i++) {
+                    System.out.println((i + 1) + "등: " + scores[i]);
+                }
+            } else if (selectNo == 5) {
+
+                run = false;
+            }
+        }
     }
 }
