e03b0e65c93d85b190a2f17b0e75c5663030e3d2
leesuyeon116 <suyeon873088@naver.com>
2023-12-12 14:41:53 +0900

    객체지향-method 연습

---
+++ src/ch10_class/ex7/CalculatorMain.java
@@ -0,0 +1,11 @@
+package ch10_class.ex7;
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+        /**
+         * 실행하면 반복문으로 메뉴를 출력
+         * 1.덧셈 2.뺄셈 3.곱셈 4.나눗셈
+         * 각 번호를 선택하면 Calculator 클래스에 있는 해당 메서드를 호출하여 결과를 출력함
+         */
+    }
+}
