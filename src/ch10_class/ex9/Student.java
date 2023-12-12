b19cb99151f3baa612943d79092df18b9f15ef12
leesuyeon116 <suyeon873088@naver.com>
2023-12-12 15:39:38 +0900

    오늘 과제

---
+++ src/ch10_class/ex9/Student.java
@@ -0,0 +1,19 @@
+package ch10_class.ex9;
+
+public class Student {
+    /*
+    필드
+    이름(StudentName)
+    학번(StudentNumber)
+    학과(StudentMajor)
+    전화번호(StudentMobile)
+     */
+
+    // 각 필드의 getter/setter 메서드
+
+    // 기본생성자
+
+    // 모든 필드를 매개변수로 하는 생성자
+
+    // 필드에 저장된 값을 출력하는 print() 메서드
+}
---
+++ src/ch10_class/ex9/StudentMain.java
@@ -0,0 +1,11 @@
+package ch10_class.ex9;
+
+public class StudentMain {
+    public static void main(String[] args) {
+        // 기본생성자로 Student 클래스 객체를 만들고 setter를 이용하여 필드값 채우기
+
+        // 모든 필드를 매개변수로 하는 생성자로 Student 클래스 객체 만들기
+
+        // 각각ㄱ의 객체에서 print 메서드 호출하여 필드값 출력
+    }
+}
