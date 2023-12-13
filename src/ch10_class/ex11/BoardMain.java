cb65e277b8136b299703387f9242cb1250f85d21
leesuyeon116 <suyeon873088@naver.com>
2023-12-13 15:38:00 +0900

    클래스연습-게시판완료~

--- src/ch10_class/ex11/BoardMain.java
+++ src/ch10_class/ex11/BoardMain.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class BoardMain {
     public static void main(String[] args) {
-        /*
+        /**
          * 메뉴
          * 1. 글작성(매개변수 있는 생성자이용)
          *      글작성 정보는 스캐너로 받음. 글번호도 같이 입력함.
@@ -18,16 +18,25 @@ public class BoardMain {
         Board board = null;
 
         while (run) {
-            System.out.println("-------------------------------------------");
+            System.out.println("--------------------------------------------------------------");
             System.out.println("1.글작성 | 2.글조회 | 3.종료");
-            System.out.println("-------------------------------------------");
+            System.out.println("--------------------------------------------------------------");
             System.out.print("선택> ");
             selectNo = scanner.nextInt();
             if (selectNo == 1) {
-
-
+                System.out.print("글번호: ");
+                Long id = scanner.nextLong();
+                System.out.print("제목: ");
+                String boardTitle = scanner.next();
+                System.out.print("작성자: ");
+                String boardWriter = scanner.next();
+                System.out.print("내용: ");
+                String boardContents = scanner.next();
+                board = new Board(id, boardTitle, boardWriter, boardContents);
             } else if (selectNo == 2) {
-
+                System.out.print("조회할 글번호: ");
+                Long id = scanner.nextLong();
+                board.findById(id);
             } else if (selectNo == 3) {
                 run = false;
             }
