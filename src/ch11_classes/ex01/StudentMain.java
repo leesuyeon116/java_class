d97f0da753a540f5bc3f05ef365b2226f4e8343c
leesuyeon116 <suyeon873088@naver.com>
2023-12-15 10:22:44 +0900

    클래스 분리 예제

--- src/ch11_classes/ex01/StudentMain.java
+++ src/ch11_classes/ex01/StudentMain.java
@@ -1,11 +1,14 @@
-package ch11_classes;
+package ch11_classes.ex01;
 
 import java.util.Scanner;
+
 public class StudentMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         boolean run = true;
         int selectNo = 0;
+        // StudentService 클래스 객체 선언
+        StudentService studentService = new StudentService();
 
         while (run) {
             System.out.println("--------------------------------------------------------------");
@@ -14,11 +17,13 @@ public class StudentMain {
             System.out.print("선택> ");
             selectNo = scan.nextInt();
             if (selectNo == 1) {
-
+                System.out.println("메서드 호출 전");
+                studentService.method1();
+                System.out.println("메서드 호출 후");
             } else if (selectNo == 2) {
-
+                studentService.method2();
             } else if (selectNo == 3) {
-
+                studentService.method3();
             } else if (selectNo == 4) {
                 run = false;
             }
--- src/ch11_classes/ex01/StudentService.java
+++ src/ch11_classes/ex01/StudentService.java
@@ -1,4 +1,5 @@
-package ch11_classes;
+package ch11_classes.ex01;
+
 public class StudentService {
     StudentRepository studentRepository = new StudentRepository();
     /**
@@ -18,8 +19,23 @@ public class StudentService {
         studentRepository.method2(str1);
     }
 
+    /**
+     * StudentDTO 객체를 생성하고
+     * Repository의 method3로 DTO 객체를 전달(호출)함.
+     */
     public void method3() {
         System.out.println("StudentService.method3");
+        StudentDTO studentDTO = new StudentDTO("학생1", "20231111", "컴퓨터공학", "010-1111-1111");
+//        StudentDTO studentDTO = new StudentDTO();
+        boolean result = studentRepository.method3(studentDTO);
+        System.out.println("result = " + result);
+        if (result) {
+            System.out.println("학생등록 성공");
+        } else {
+            System.out.println("학생등록 실패");
+        }
     }
+
+
 
 }
