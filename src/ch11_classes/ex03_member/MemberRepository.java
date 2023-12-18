f7bd22aea08b1210facd7c4ddd0c9fad9be8060e
leesuyeon116 <suyeon873088@naver.com>
2023-12-18 14:23:17 +0900

    회원 프로젝트 - 로그인 기능 완료

--- src/ch11_classes/ex03_member/MemberRepository.java
+++ src/ch11_classes/ex03_member/MemberRepository.java
@@ -9,4 +9,13 @@ public class MemberRepository {
     private boolean sare(MemberDTO memberDTO) {
         return memberDTOList.add(memberDTO);
     }
+    public MemberDTO login(String memberEmail, String memberPassword) {
+        MemberDTO memberDTO = null;
+        for (int i = 0; i < memberDTOList.size(); i++) {
+            if (memberEmail.equals(memberDTOList.get(i).getMemberEmail()) && memberPassword.equals(memberDTOList.get(i).getMemberPassword())) {
+                memberDTO = memberDTOList.get(i);
+            }
+        }
+        return memberDTO;
+    }
 }
--- src/ch11_classes/ex03_member/MemberService.java
+++ src/ch11_classes/ex03_member/MemberService.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class MemberService {
     Scanner scanner = new Scanner(System.in);
     MemberRepository memberRepository = new MemberRepository();
+    private static String loginEmail = null;
 
     public void save() {
         System.out.print("이메일: ");
@@ -21,6 +22,21 @@ public class MemberService {
             System.out.println("회원가입 성공");
         } else {
             System.out.println("회원가입 실패");
+        }
+    }
+
+    public void login() {
+        System.out.print("이메일: ");
+        String memberEmail = scanner.next();
+        System.out.print("비밀번호: ");
+        String memberPassword = scanner.next();
+        MemberDTO loginResult = memberRepository.login(memberEmail, memberPassword);
+        if (loginResult != null) {
+            System.out.println("로그인 성공");
+            System.out.println(memberEmail + "님 환영합니다!");
+            loginEmail = memberEmail;
+        } else {
+            System.out.println("이메일 또는 비밀번호가 틀립니다ㅠㅠ");
         }
     }
 }
