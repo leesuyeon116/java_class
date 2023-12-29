d79b4370bf74bf53cbcec7040c33525ac74a2e6a
leesuyeon116 <suyeon873088@naver.com>
2023-12-29 16:10:41 +0900

    집 가서 다 뜯어고쳐야 함ㅠㅠ 일단 커밋..

--- src/ch16_bookpang/MainController.java
+++ src/ch16_bookpang/MainController.java
@@ -1,4 +1,163 @@
 package ch16_bookpang;
 
+import ch16_bookpang.service.BookService;
+import ch16_bookpang.service.MemberService;
+
+import java.util.Scanner;
+
 public class MainController {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean run = true;
+        int selectNo;
+
+        BookService bookService = new BookService();
+        MemberService memberService = new MemberService();
+
+        while (run) {
+            System.out.println("=== 메인 메뉴 ===");
+            System.out.println("1. 도서 서비스");
+            System.out.println("2. 회원 서비스");
+            System.out.println("3. 종료");
+            System.out.print("서비스를 선택하세요 (1-3): ");
+
+            try {
+                selectNo = scanner.nextInt();
+                scanner.nextLine(); // 개행 문자 처리
+            } catch (Exception e) {
+                System.out.println("잘못된 입력입니다. 숫자를 입력하세요.");
+                scanner.nextLine(); // 유효하지 않은 입력 처리
+                continue;
+            }
+
+            switch (selectNo) {
+                case 1:
+                    runBookService(scanner, bookService);
+                    break;
+                case 2:
+                    runMemberService(scanner, memberService);
+                    break;
+                case 3:
+                    System.out.println("프로그램을 종료합니다. 안녕히 계세요!");
+                    run = false;
+                    break;
+                default:
+                    System.out.println("잘못된 선택입니다. 올바른 숫자를 입력하세요.");
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static void runBookService(Scanner scanner, BookService bookService) {
+        boolean runBookService = true;
+        int bookServiceOption;
+
+        while (runBookService) {
+            System.out.println("=== 도서 서비스 메뉴 ===");
+            System.out.println("1. 도서 추가");
+            System.out.println("2. 모든 도서 조회");
+            System.out.println("3. 도서 검색");
+            System.out.println("4. 도서 가격 수정");
+            System.out.println("5. 도서 삭제");
+            System.out.println("6. 카트에 추가");
+            System.out.println("7. 카트 조회");
+            System.out.println("8. 카트에서 삭제");
+            System.out.println("9. 메인 메뉴로 돌아가기");
+            System.out.print("옵션을 선택하세요 (1-9): ");
+
+            try {
+                bookServiceOption = scanner.nextInt();
+                scanner.nextLine(); // 개행 문자 처리
+            } catch (Exception e) {
+                System.out.println("잘못된 입력입니다. 숫자를 입력하세요.");
+                scanner.nextLine(); // 유효하지 않은 입력 처리
+                continue;
+            }
+
+            switch (bookServiceOption) {
+                case 1:
+                    bookService.save();
+                    break;
+                case 2:
+                    bookService.findAll();
+                    break;
+                case 3:
+                    bookService.search();
+                    break;
+                case 4:
+                    bookService.update();
+                    break;
+                case 5:
+                    bookService.delete();
+                    break;
+                case 6:
+                    bookService.addToCart();
+                    break;
+                case 7:
+                    bookService.viewCart();
+                    break;
+                case 8:
+                    bookService.removeFromCart();
+                    break;
+                case 9:
+                    runBookService = false;
+                    break;
+                default:
+                    System.out.println("잘못된 선택입니다. 올바른 숫자를 입력하세요.");
+            }
+        }
+    }
+
+    private static void runMemberService(Scanner scanner, MemberService memberService) {
+        boolean runMemberService = true;
+        int memberServiceOption;
+
+        while (runMemberService) {
+            System.out.println("=== 회원 서비스 메뉴 ===");
+            System.out.println("1. 회원 가입");
+            System.out.println("2. 로그인");
+            System.out.println("3. 모든 회원 조회");
+            System.out.println("4. 회원 정보 수정");
+            System.out.println("5. 회원 계정 삭제");
+            System.out.println("6. 로그아웃");
+            System.out.println("7. 메인 메뉴로 돌아가기");
+            System.out.print("옵션을 선택하세요 (1-7): ");
+
+            try {
+                memberServiceOption = scanner.nextInt();
+                scanner.nextLine(); // 개행 문자 처리
+            } catch (Exception e) {
+                System.out.println("잘못된 입력입니다. 숫자를 입력하세요.");
+                scanner.nextLine(); // 유효하지 않은 입력 처리
+                continue;
+            }
+
+            switch (memberServiceOption) {
+                case 1:
+                    memberService.save();
+                    break;
+                case 2:
+                    memberService.login();
+                    break;
+                case 3:
+                    memberService.findAll();
+                    break;
+                case 4:
+                    memberService.update();
+                    break;
+                case 5:
+                    memberService.delete();
+                    break;
+                case 6:
+                    memberService.logout();
+                    break;
+                case 7:
+                    runMemberService = false;
+                    break;
+                default:
+                    System.out.println("잘못된 선택입니다. 올바른 숫자를 입력하세요.");
+            }
+        }
+    }
 }
--- src/ch16_bookpang/service/BookService.java
+++ src/ch16_bookpang/service/BookService.java
@@ -2,14 +2,17 @@ package ch16_bookpang.service;
 
 
 import ch16_bookpang.dto.BookDTO;
+import ch16_bookpang.dto.CartDTO;
 import ch16_bookpang.repository.BookRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class BookService {
     Scanner scanner = new Scanner(System.in);
     BookRepository bookRepository = new BookRepository();
+    private List<CartDTO> cart = new ArrayList<>(); // 카트 리스트
     public void save() {
         System.out.print("도서명: ");
         String bookTitle = scanner.next();
@@ -91,6 +94,57 @@ public class BookService {
             System.out.println("삭제 성공");
         } else {
             System.out.println("삭제 실패");
+        }
+    }
+    //  카트 도서 추가 메서드
+    public void addToCart() {
+        System.out.println("추가할 도서의 id: ");
+        Long bookId = scanner.nextLong();
+        System.out.println("추가할 수량: ");
+        int quantity = scanner.nextInt();
+
+        BookDTO bookDTO = bookRepository.findById(bookId);
+        if (bookDTO != null) {
+            int totalPrice = bookDTO.getBookPrice() * quantity;
+            CartDTO cartItem = new CartDTO(bookId, bookDTO.getBookTitle(), quantity, totalPrice);
+            cart.add(cartItem);
+            System.out.println("카트에 도서가 추가되었습니다.");
+        } else {
+            System.out.println("도서를 찾을 수 없습니다.");
+        }
+    }
+
+    //  카트 내용 조회 메서드
+    public void viewCart() {
+        if (cart.isEmpty()) {
+            System.out.println("카트가 비어 있습니다.");
+        } else {
+            System.out.println("카트 내용:");
+            for (CartDTO cartItem : cart) {
+                System.out.println(cartItem);
+            }
+        }
+    }
+    //  카트 도서 제거 메서드
+    public void removeFromCart() {
+        if (cart.isEmpty()) {
+            System.out.println("카트가 비어 있습니다.");
+        } else {
+            System.out.print("삭제할 도서의 id: ");
+            Long bookId = scanner.nextLong();
+
+            // Iterator를 사용하여 도서 ID에 해당하는 아이템을 찾아 제거
+            // Iterator를 사용하는 이유는 중간에 아이템을 제거할 때 ConcurrentModificationException을 방지하기 위함
+            for (var iterator = cart.iterator(); iterator.hasNext(); ) {
+                CartDTO cartItem = iterator.next();
+                if (cartItem.getBookId().equals(bookId)) {
+                    iterator.remove();
+                    System.out.println("카트에서 도서가 제거되었습니다.");
+                    return;
+                }
+            }
+
+            System.out.println("해당 id의 도서를 찾을 수 없습니다.");
         }
     }
 
