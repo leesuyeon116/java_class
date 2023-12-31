9a21f604823f57fe86e8fe8ed2559fe0c53c212e
leesuyeon116 <suyeon873088@naver.com>
2024-01-01 00:03:50 +0900

    24년 첫 커밋(근데 오류 많음)

--- src/ch17_bookpang/repository/BookRepository.java
+++ src/ch17_bookpang/repository/BookRepository.java
@@ -1,4 +1,68 @@
 package ch17_bookpang.repository;
 
+import ch17_bookpang.dto.BookDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookRepository {
+    private static List<BookDTO> bookDTOList = new ArrayList<>();
+    // 도서목록 메서드
+    private List<BookDTO> findAll() {
+        return bookDTOList;
+    }
+
+    // 도서조회 메서드
+    private BookDTO findId(Long id) {
+        BookDTO bookDTO = null;
+        for (BookDTO bookDTO1: bookDTOList) {
+            if (id.equals(bookDTO1.getId())) {
+            }
+        }
+        return bookDTO;
+    }
+    public BookDTO findByTitle(String bookTitle) {
+        BookDTO bookDTO = null;
+        for (int i = 0; i < bookDTOList.size(); i++) {
+            if (bookTitle.equals(bookDTOList.get(i).getBookTitle())) {
+                bookDTO = bookDTOList.get(i);
+            }
+        }
+        return bookDTO;
+    }
+
+    public List<BookDTO> search(String bookTitle) {
+        //  검색결과 담을 Lsit 선언
+        List<BookDTO> bookDTOS = new ArrayList<>();
+        for (int i = 0; i < bookDTOList.size(); i++) {
+            // 저장되어 있는 도서명에 검색어가 포함되어 있으면 true
+            if (bookDTOList.get(i).getBookTitle().contains(bookTitle)) {
+                // 조건을 만족하면 bookDTOS 에 추가
+                BookDTO bookDTO = bookDTOList.get(i);
+                bookDTO.add(bookDTO);
+            }
+        }
+        return bookDTOS;
+    }
+
+    public boolean update(Long id, int bookTitle) {
+        boolean result = false;
+        for (int i = 0; i < bookDTOList.size(); i++) {
+            if (id.equals(bookDTOList.get(i).getId())) {
+                bookDTOList.get(i).setBookPrice(bookTitle);
+                result = true;
+            }
+        }
+        return result;
+    }
+    public boolean delete(Long id) {
+        boolean result = false;
+        for (int i = 0; i < bookDTOList.size(); i++) {
+            if (id.equals(bookDTOList.get(i).getId())) {
+                bookDTOList.remove(i);
+                result = true;
+            }
+        }
+        return result;
+    }
 }
--- src/ch17_bookpang/service/BookService.java
+++ src/ch17_bookpang/service/BookService.java
@@ -1,4 +1,102 @@
 package ch17_bookpang.service;
 
+import ch17_bookpang.dto.BookDTO;
+import ch17_bookpang.repository.BookRepository;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class BookService {
+    Scanner scanner = new Scanner(System.in);
+    BookRepository bookRepository = new BookRepository();
+
+    // 도서등록 메서드
+    public void save() {
+        System.out.println("도서명: ");
+        String bookTitle = scanner.next();
+        System.out.println("저자: ");
+        String bookAuthor = scanner.next();
+        System.out.println("가격: ");
+        int bookPrice = scanner.nextInt();
+        System.out.println("출판사: ");
+        String bookPublisher = scanner.next();
+        BookDTO bookDTO = new BookDTO(bookTitle, bookAuthor, bookPrice, bookPublisher);
+        boolean result = bookRepository.save(bookDTO);
+        if (result) {
+            System.out.println("등록 성공");
+        } else {
+            System.out.println("등록 실패");
+        }
+    }
+    // 도서목록 메서드
+    public void findAll() {
+        List<BookDTO> bookDTOList = bookRepository.findAll();
+        for (BookDTO bookDTO: bookDTOList) {
+            System.out.println("bookDTO = " + bookDTO);
+        }
+    }
+
+    // 도서조회 메서드
+    public void findById() {
+        System.out.println("조회 id: ");
+        Long id = scanner.nextLong();
+        BookDTO bookDTO = bookRepository.findById(id);
+        if (bookDTO != null) {
+            System.out.println("bookDTO = " + bookDTO);
+        } else {
+            System.out.println("조회결과가 없습니다.");
+        }
+    }
+
+    public void findByTitle() {
+        System.out.println("도서명: ");
+        String bookTitle = scanner.next();
+        BookDTO bookDTO = bookRepository.findByTitle(bookTitle);
+        if (bookDTO != null) {
+            System.out.println("bookDTO = " + bookDTO);
+        } else {
+            System.out.println("조회결과가 없습니다.");
+        }
+    }
+
+    public void search() {
+        System.out.println("검색어: ");
+        String bookTitle = scanner.next();
+        List<BookDTO> bookDTOList = bookRepository.search(bookTitle);
+        if (bookDTOList.size() > 0) {
+            for (BookDTO bookDTO: bookDTOList) {
+                System.out.println("bookDTO = " + bookDTO);
+            }
+        } else {
+            System.out.println("검색 결과가 없습니다.");
+        }
+    }
+    public void update() {
+        System.out.print("수정할 id: ");
+        Long id = scanner.nextLong();
+        BookDTO bookDTO = bookRepository.findById(id);
+        if (bookDTO != null) {
+            System.out.print("수정할 가격: ");
+            int bookPrice = scanner.nextInt();
+            boolean updateResult = bookRepository.update(id, bookPrice);
+            if (updateResult) {
+                System.out.println("수정 성공");
+            } else {
+                System.out.println("수정 실패");
+            }
+        } else {
+            System.out.println("조회 결과가 없습니다.");
+        }
+    }
+
+    public void delete() {
+        System.out.println("삭제할 id: ");
+        Long id = scanner.nextLong();
+        boolean result = bookRepository.delete(id);
+        if (result) {
+            System.out.println("삭제 성공");
+        } else {
+            System.out.println("삭제 실패");
+        }
+    }
 }
