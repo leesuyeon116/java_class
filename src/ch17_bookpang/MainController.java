3cfa7f81217794472715600137ec74fd0088224f
leesuyeon116 <suyeon873088@naver.com>
2024-01-01 22:19:27 +0900

    하는 중..

--- src/ch17_bookpang/MainController.java
+++ src/ch17_bookpang/MainController.java
@@ -1,4 +1,47 @@
 package ch17_bookpang;
 
+import ch17_bookpang.service.BookService;
+import ch17_bookpang.service.MemberService;
+
+import java.util.Scanner;
+
 public class MainController {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean run = true;
+        int selectNo = 0;
+        BookService bookService = new BookService();
+        MemberService memberService = new MemberService();
+        while (run) {
+            System.out.println("======== 회원 ========");
+            System.out.println("----------------------------------------------------------------------------------------------------");
+            System.out.println("1. 회원가입 | 2. 로그인 | 3. 회원수정 | 4. 회원탈퇴 | 5. 로그아웃 | 6. 도서조회(id) | 7. 도서조회(제목) | 0. 종료");
+            System.out.println("선택-> ");
+            selectNo = scanner.nextInt();
+            if (selectNo == 1) {
+                System.out.println("회원가입");
+                // 호출하는 문장 작성
+            } else if (selectNo == 2) {
+                System.out.println("로그인");
+                // 호출하는 문장 작성
+            } else if (selectNo == 3) {
+                System.out.println("회원수정");
+                // 호출하는 문장 작성
+            } else if (selectNo == 4) {
+                System.out.println("회원탈퇴");
+                // 호출하는 문장 작성
+            } else if (selectNo == 5) {
+                System.out.println("로그아웃");
+                // 호출하는 문장 작성
+            } else if (selectNo == 6) {
+                System.out.println("도서조회(id)");
+                // 호출하는 문장 작성
+            } else if (selectNo == 7) {
+                System.out.println("도서조회(제목)");
+                // 호출하는 문장 작성
+            } else if (selectNo == 0) {
+                System.out.println("종료");
+            }
+        }
+    }
 }
--- src/ch17_bookpang/repository/MemberRepository.java
+++ src/ch17_bookpang/repository/MemberRepository.java
@@ -1,4 +1,54 @@
 package ch17_bookpang.repository;
 
+import ch17_bookpang.dto.MemberDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MemberRepository {
+    private static List<MemberDTO>memberDTOList = new ArrayList<>();
+    public boolean save(MemberDTO memberDTO) {
+        return memberDTOList.add(memberDTO);
+    }
+    public MemberDTO login(String memberEmail, String memberPassword) {
+        MemberDTO memberDTO = null;
+        for (int i = 0; i < memberDTOList.size(); i++) {
+            if (memberEmail.equals(memberDTOList.get(i).getMemberEmail()) && memberPassword.equals(memberDTOList.get(i).getMemberPassword())) {
+                memberDTO = memberDTOList.get(i);
+            }
+        }
+        return memberDTO;
+    }
+    public List<MemberDTO> findAll() {
+        return memberDTOList;
+    }
+    public boolean update(String loginEmail, String mobile) {
+        boolean result = false;
+        for (int i = 0; i < memberDTOList.size(); i++) {
+            if (loginEmail.equals(memberDTOList.get(i).getMemberEmail())) {
+                memberDTOList.get(i).setMemberMobile(mobile);
+                result = true;
+            }
+        }
+        return result;
+    }
+    public boolean deldte(String loginEmail) {
+        boolean result= false;
+        for (int i = 0; i < memberDTOList.size(); i++) {
+            if (loginEmail.equals(memberDTOList.get(i).getMemberEmail())) {
+                memberDTOList.remove(i);
+                result = true;
+            }
+        }
+        return result;
+    }
+    public boolean emailCheck(String memberEmail) {
+        boolean result = true;
+        for (int i = 0; i < memberDTOList.size(); i++) {
+            if (memberEmail.equals(memberDTOList.get(i).getMemberEmail())) {
+                result = false;
+            }
+        }
+        return result;
+    }
 }
